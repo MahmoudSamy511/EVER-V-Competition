b89499c0d9c1874417caff20188acfc15ad06148
EssamShenhab <152318011+EssamShenhab@users.noreply.github.com>
2024-03-03 20:34:26 +0300

    Add files via upload

---
+++ Essam_Shenhab/switch_lane.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float64
+import time
+
+def send_commands():
+    rospy.init_node('simple_car_controller', anonymous=True)
+
+    # Define ROS publishers
+
+    cmd_vel_pub = rospy.Publisher('/cmd_vel', Float64, queue_size=10)
+    brakes_pub = rospy.Publisher('/brakes', Float64, queue_size=10)
+    steering_pub = rospy.Publisher('/SteeringAngle', Float64, queue_size=10)
+    
+    rospy.sleep(1)  # Wait for publishers to register
+    
+    # Send gas pedal command and start turning
+    brakes_msg = Float64()
+    brakes_msg.data = 0.0
+    brakes_pub.publish(brakes_msg)
+
+    gas_pedal_msg = Float64()
+    gas_pedal_msg.data = 0.2
+    cmd_vel_pub.publish(gas_pedal_msg)
+    
+    steering_msg = Float64()
+    steering_msg.data = 0.0  # Steering angle for turning (adjust as needed)
+    steering_pub.publish(steering_msg)
+
+    rospy.sleep(13)
+
+    steering_msg = Float64()
+    steering_msg.data = 9.0  # Steering angle for turning (adjust as needed)
+    steering_pub.publish(steering_msg)
+
+
+    rospy.sleep(2)
+
+    steering_msg = Float64()
+    steering_msg.data = -9.0  # Steering angle for turning (adjust as needed)
+    steering_pub.publish(steering_msg)
+
+    rospy.sleep(2)
+
+    brakes_msg = Float64()
+    brakes_msg.data = 0.0
+    brakes_pub.publish(brakes_msg)
+
+    gas_pedal_msg = Float64()
+    gas_pedal_msg.data = 0.2
+    cmd_vel_pub.publish(gas_pedal_msg)
+    
+    steering_msg = Float64()
+    steering_msg.data = 0.0  # Steering angle for turning (adjust as needed)
+    steering_pub.publish(steering_msg)
+
+    rospy.sleep(11.5)
+
+    # Stop turning and apply brakes
+    steering_pub.publish(Float64())  # Stop turning
+    brakes_msg.data = 0.65
+    cmd_vel_pub.publish(Float64())  # Stop gas pedal
+    brakes_pub.publish(brakes_msg)
+
+
+if __name__ == '__main__':
+    try:
+        send_commands()
+    except rospy.ROSInterruptException:
+        pass
