741e097acf0b5c5b1085367570e2d6701a98e853
Essam_Shenhab <essam119999@gmail.com>
2024-03-06 13:49:21 +0200

    updated the plot.png

--- Essam_Shenhab/Milestone_One/Task1(straight_line)/straight_line.py
+++ Essam_Shenhab/Milestone_One/Task1(straight_line)/straight_line.py
@@ -25,7 +25,7 @@ def straight_line():
     steering_msg.data = 0.0  # Steering angle for turning (adjust as needed)
     steering_pub.publish(steering_msg)
 
-    rospy.sleep(13.225)
+    rospy.sleep(13.12)
 
     # Stop turning and apply brakes
     steering_pub.publish(Float64())  # Stop turning
