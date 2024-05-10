54860308293f45bb97712e9d2b32574cf305ce96
Mahmoud Samy <mahmoudsamyzx11@gmail.com>
2024-05-11 00:07:40 +0300

    Upload Milestone1 Submission Files

---
+++ Milestone_1/Task3(one_circle)/one_circle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float64
+import time
+
+def one_circle():
+    # Define ROS publishers
+    cmd_vel_pub = rospy.Publisher('/cmd_vel', Float64, queue_size=10)
+    brakes_pub = rospy.Publisher('/brakes', Float64, queue_size=10)
+    steering_pub = rospy.Publisher('/SteeringAngle', Float64, queue_size=10)
+    
+    rospy.sleep(1)  # Wait for publishers to register
+    
+    # Send gas pedal command and start turning
+    brakes_msg = Float64()
+    brakes_msg.data = 0.0
+    brakes_pub.publish(brakes_msg)
+
+    gas_pedal_msg = Float64()
+    gas_pedal_msg.data = 0.2
+    cmd_vel_pub.publish(gas_pedal_msg)
+    
+    steering_msg = Float64()
+    steering_msg.data = 17.9597315  # Steering angle for turning (adjust as needed)
+    steering_pub.publish(steering_msg)
+    
+    rospy.sleep(8.986)  # Sleep for 2 seconds
+    
+    # Stop turning and apply brakes
+    brakes_msg.data = 0.5211
+    cmd_vel_pub.publish(Float64())  # Stop gas pedal
+    brakes_pub.publish(brakes_msg)
+
+    rospy.sleep(3.2)
+    
+
+if __name__ == '__main__':
+    try:
+        one_circle()
+    except rospy.ROSInterruptException:
+        pass
