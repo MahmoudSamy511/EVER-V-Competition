a14ed37ec3fcb63c3a3d55ec431815388970955d
M0stafa077 <ma4190243@gmail.com>
2024-05-11 04:57:29 +0300

    switch_lane path is now compatible with C-L controller

--- milestone_2/open-loop-controller/Task2(switch_lane)/main_switch_lane.py
+++ milestone_2/open-loop-controller/Task2(switch_lane)/main_switch_lane.py
@@ -7,7 +7,6 @@ import csv
 import os
 from datetime import datetime
 from switch_lane import switch_lane
-from charts import main as plot
 
 class OdomImuLogger:
     def __init__(self):
@@ -15,26 +14,15 @@ class OdomImuLogger:
         self.imu_data = []
 
         # Initialize ROS node
-        rospy.init_node('odom_imu_logger', anonymous=True)
+        rospy.init_node('odom_logger', anonymous=True)
 
         # Subscribe to odom topic
         rospy.Subscriber('/odom', Odometry, self.odom_callback)
 
-        # Subscribe to imu topic
-        rospy.Subscriber('/Imu', Imu, self.imu_callback)
-
     def odom_callback(self, msg):
         # Process and store odometry data
-        time = datetime.fromtimestamp(rospy.Time.now().to_sec()).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-        odom_values = [time, msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z]
+        odom_values = [msg.pose.pose.position.x, msg.pose.pose.position.y]
         self.odom_data.append(odom_values)
-
-
-    def imu_callback(self, msg):
-        # Process and store IMU data
-        time = datetime.fromtimestamp(rospy.Time.now().to_sec()).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-        imu_values = [time, msg.linear_acceleration.x, msg.linear_acceleration.y, msg.linear_acceleration.z, msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z]
-        self.imu_data.append(imu_values)
 
     def save_to_csv(self):
         # Create directory for saving CSV files if not exists
@@ -44,20 +32,13 @@ class OdomImuLogger:
         # Write odom data to CSV
         with open("data/odom_data.csv", "w", newline="") as f:
             writer = csv.writer(f)
-            writer.writerow(["Timestamp", "X", "Y", "Z"])
+            writer.writerow(["X", "Y"])
             writer.writerows(self.odom_data)
-
-        # Write imu data to CSV
-        with open("data/imu_data.csv", "w", newline="") as f:
-            writer = csv.writer(f)
-            writer.writerow(["Timestamp", "Linear_Acceleration_X", "Linear_Acceleration_Y", "Linear_Acceleration_Z", "Orientation_X", "Orientation_Y", "Orientation_Z", "Angular_Velocity_X", "Angular_Velocity_Y", "Angular_Velocity_Z"])
-            writer.writerows(self.imu_data)
 
 def main():
     logger = OdomImuLogger()
     switch_lane()
     logger.save_to_csv()
-    plot()
 
 if __name__ == '__main__':
     main()
